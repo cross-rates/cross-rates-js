b9ab0e8c7831aad6b1af04bcda8b4a6ff668d5bd
Serg-Maximchuk <serg.maximchuk@gmail.com>
2020-07-18 23:33:32 +0300

    Cleanup

--- test/components/binanceApiClient.test.js
+++ test/components/binanceApiClient.test.js
@@ -2,7 +2,7 @@ import {expect, test} from "@jest/globals";
 import {binanceApiClient} from "../../src/components/binanceApiClient";
 
 
-test('can load data 2', async () => {
+test('can load data', async () => {
     await binanceApiClient.fetchCryptoCurrencies(
         c => expect(c).toBeDefined(), console.error
     )
