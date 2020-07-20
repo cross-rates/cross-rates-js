1a3dab708c70780b0394466d8c76ee7a7d69d888
Serg-Maximchuk <serg.maximchuk@gmail.com>
2020-07-20 10:01:26 +0300

    No more fetching rates on start

--- cross-rates-browser/src/index.js
+++ cross-rates-browser/src/index.js
@@ -3,5 +3,3 @@ import {LocalStorageRepository} from "./components/LocalStorageRepository";
 
 export const rates = ratesWithStorage(() => LocalStorageRepository.builder());
 
-rates.refreshRates();
-
